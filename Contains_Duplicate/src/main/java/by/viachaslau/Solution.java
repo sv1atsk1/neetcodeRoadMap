ddd78f861d5ca179be69cf2d81440f24a1ce5cd6
Viachaslau <12170068@study.bsuir.by>
2025-03-03 19:32:29 +0300

    Adding the time and space complexity for solution

--- Contains_Duplicate/src/main/java/by/viachaslau/Solution.java
+++ Contains_Duplicate/src/main/java/by/viachaslau/Solution.java
@@ -2,6 +2,8 @@ package by.viachaslau;
 
 import java.util.HashSet;
 
+//Временная сложность O(n): Мы проходим по массиву nums один раз, где n — длина массива.
+//Пространственная сложность O(n): В худшем случае, если в массиве нет дубликатов, мы добавим все n элементов в HashSet.
 public class Solution {
     public boolean hasDuplicate(int[] nums) {
         HashSet<Integer> hashSet = new HashSet<>();
