de3c62f184cd5b190db4cfd2d742ac88c410e969
Viachaslau <12170068@study.bsuir.by>
2025-03-10 11:58:04 +0300

    quick fix

--- Two Pointers/ThreeSum/src/main/java/by/viachaslau/Solution.java
+++ Two Pointers/ThreeSum/src/main/java/by/viachaslau/Solution.java
@@ -8,13 +8,13 @@ import java.util.List;
 //Space Complexity: O(1) for the pointers used, but O(n) can be considered for the output list.
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < nums.length - 2; i++) {
 
+        for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1, right = nums.length - 1;
-
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
 
