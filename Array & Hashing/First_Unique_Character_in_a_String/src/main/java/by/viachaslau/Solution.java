50840bce1720971f9b79bfc1794008f2e8ee0f9f
Viachaslau <12170068@study.bsuir.by>
2025-04-15 14:41:40 +0300

    First_Unique_Character_in_a_String Solution

---
+++ Array & Hashing/First_Unique_Character_in_a_String/src/main/java/by/viachaslau/Solution.java
@@ -0,0 +1,21 @@
+package by.viachaslau;
+
+import java.util.HashMap;
+
+public class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (charCount.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
