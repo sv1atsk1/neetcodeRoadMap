30ee4a21d0aee8caf46b32a2af3fc395a439392c
kN1GGhT <sv1atsk1@mail.ru>
2025-04-19 13:52:21 +0300

    Find_Minimum_in_Rotated_Sorted_Array Solution

---
+++ BinarySearch/Find_Minimum_in_Rotated_Sorted_Array/src/main/java/by/viachaslau/Solution.java
@@ -0,0 +1,26 @@
+package by.viachaslau;
+
+public class Solution {
+    public int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        int result = nums[0];
+
+        while (l <= r) {
+
+            if (nums[l] < nums[r]) {
+                result = Math.min(result, nums[l]);
+                break;
+            }
+
+            int m = l + (r - l) / 2;
+            result = Math.min(result, nums[m]);
+            if (nums[m] >= nums[l]) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return result;
+    }
+}
