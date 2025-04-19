8b63ce3e1bc6fa734d75baf1629a0cb785e4b8a6
kN1GGhT <sv1atsk1@mail.ru>
2025-04-19 14:06:46 +0300

    Search_in_Rotated_Sorted_Array Solution

---
+++ BinarySearch/Search_in_Rotated_Sorted_Array/src/main/java/by/viachaslau/Solution.java
@@ -0,0 +1,37 @@
+package by.viachaslau;
+
+//Space Complexity - 0(1);
+//Time Complexity - 0(log(n));
+
+public class Solution {
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
