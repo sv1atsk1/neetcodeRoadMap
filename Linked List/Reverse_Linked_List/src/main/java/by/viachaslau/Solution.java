df005303084ab2e2b9a8514b766d4523c66e7fdd
kN1GGhT <sv1atsk1@mail.ru>
2025-04-20 22:18:34 +0300

    Reverse_Linked_List Solution

---
+++ Linked List/Reverse_Linked_List/src/main/java/by/viachaslau/Solution.java
@@ -0,0 +1,37 @@
+package by.viachaslau;
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+//Space complexity: O(1)
+//Time complexity: O(n)
+
+class Solution {
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
