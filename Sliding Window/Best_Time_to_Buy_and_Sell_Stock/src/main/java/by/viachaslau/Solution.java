f34da652df99afb2a6f2e117c1486fb5e70eab9b
kN1GGhT <sv1atsk1@mail.ru>
2025-04-20 21:42:44 +0300

    Best_Time_to_Buy_and_Sell_Stock Solution

---
+++ Sliding Window/Best_Time_to_Buy_and_Sell_Stock/src/main/java/by/viachaslau/Solution.java
@@ -0,0 +1,17 @@
+package by.viachaslau;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int maxProfit = 0;
+        for (int right = 1; right < prices.length; right++) {
+            if (prices[right] > prices[left]) {
+                maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+            }
+            else{
+                left = right;
+            }
+        }
+        return maxProfit;
+    }
+}
