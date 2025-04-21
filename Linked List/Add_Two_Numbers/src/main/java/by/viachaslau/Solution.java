2518deae9d509f9bac6e3895f67ea2f22fbd50cc
kN1GGhT <sv1atsk1@mail.ru>
2025-04-21 20:32:20 +0300

    Add_Two_Numbers Solution

---
+++ Linked List/Add_Two_Numbers/src/main/java/by/viachaslau/Solution.java
@@ -0,0 +1,50 @@
+
+//Definition for singly-linked list.
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+//Time complexity: O(m+n)
+//Space complexity:
+//O(1) extra space.
+//O(max(m,n)) for the output list.
+//Where m is the length of l1 and n is the length of l2.
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = x + y + carry;
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummyHead.next;
+    }
+}
+
